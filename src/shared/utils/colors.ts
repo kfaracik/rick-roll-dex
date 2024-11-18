08dfc5f21ae0e513f1f7e1ae884242fbe51e78bf
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-18 11:33:32 +0100

    feat(colors): add colors

---
+++ src/shared/utils/colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+  primary: '#224229',
+  background: '#F4F6F5',
+  accent: '#F89F34',
+  white: '#FFFFFF',
+  mediumGreen: '#59695C',
+  darkGreen: '#162C1B',
+  greyshGreen: '#DAE4DC',
+};
