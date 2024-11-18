7f7191f83485999bd4790e18130ed0a2547475d7
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-18 22:46:53 +0100

    feat(character-details): add character details card

--- src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.screen.tsx
+++ src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.screen.tsx
@@ -1,12 +1,91 @@
-import {Text, View} from 'react-native';
 import React from 'react';
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import {styles} from './CharacterDetails.styled';
+import {Button, Card, CategoryValueText} from '../../../../shared/comopnents';
+import {RouteProp, useRoute} from '@react-navigation/native';
+import {CharacterDetailsStackParamList} from '../../CharacterDetails.routes';
+import {DATA} from '../../../../shared/api/mock';
+
+type CharacterDetailsRouteProp = RouteProp<
+  CharacterDetailsStackParamList,
+  'CharacterDetailsScreen'
+>;
+
+const VERTICAL_MARGINS = 80;
 
 const CharacterDetailsScreen = () => {
+  const route = useRoute<CharacterDetailsRouteProp>();
+  const {id} = route?.params;
+  const {width: screenWidth} = useWindowDimensions();
+  const width = screenWidth - VERTICAL_MARGINS;
+
+  const onLikePress = () => {
+    Alert.alert('TODO: implement');
+  };
+
+  const character = DATA.find(c => c.id === id); // TODO: mock add api
+
+  if (!character) {
+    return (
+      <Card style={styles.card} contentStyle={styles.content}>
+        <Text>Character not found</Text>
+      </Card>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Implement CharacterDetailsScreen</Text>
-    </View>
+    <ScrollView>
+      <Card style={styles.card} contentStyle={styles.content}>
+        <Image
+          source={{uri: character.image}}
+          style={[styles.image, {height: width, width}]}
+        />
+        <CategoryValueText
+          category={'NAME'}
+          value={character.name}
+          valueTextStyle={styles.tile}
+        />
+        <View style={styles.row}>
+          <CategoryValueText
+            category={'STATUS'}
+            value={character.status}
+            style={styles.item}
+          />
+          <CategoryValueText
+            category={'ORIGIN'}
+            value={character.origin.name}
+            style={styles.item}
+          />
+        </View>
+        <View style={styles.row}>
+          <CategoryValueText
+            category={'SPECIES'}
+            value={character.species}
+            style={styles.item}
+          />
+          <CategoryValueText
+            category={'GENDER'}
+            value={character.gender}
+            style={styles.item}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title={'✩ ADD TO LIKED'}
+            onPress={onLikePress}
+            mode={'primary'}
+            style={styles.button}
+          />
+        </View>
+      </Card>
+    </ScrollView>
   );
 };
 
--- src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.styled.ts
+++ src/stacks/CharacterDetails/screens/CharacterDetails/CharacterDetails.styled.ts
@@ -1,9 +1,40 @@
 import {StyleSheet} from 'react-native';
+import {Colors} from '../../../../shared/utils';
 
 export const styles = StyleSheet.create({
-  container: {
+  card: {
+    margin: 16,
+    padding: 24,
+    backgroundColor: Colors.white,
+  },
+  content: {
+    flexDirection: 'column',
+    gap: 16,
+  },
+  image: {
+    borderWidth: 1,
+    borderColor: Colors.primary,
+    borderRadius: 24,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 16,
+    width: '100%',
+  },
+  tile: {
+    fontSize: 36,
+  },
+  item: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: Colors.background,
+  },
+  buttonContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  button: {
+    width: '100%',
   },
 });
