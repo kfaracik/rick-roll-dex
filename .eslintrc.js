bce11cadfd516739bc43dfc583a6329aba048aeb
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-18 11:56:06 +0100

    feat(config): add rnp, react-query libs, add project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:import/errors', 'plugin:import/warnings'],
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+  plugins: [
+    'react',
+    'react-native',
+    'simple-import-sort',
+    'check-file',
+    'import',
+    'jest',
+  ],
 };
