8bdf15f7bc56296966318079b2bff4358d21ae61
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-18 11:54:57 +0100

    feat(bottom-tab): update tab names

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-
 import {TabNavigationStack} from './src/stacks/TabNavigation';
 
 function App(): React.JSX.Element {
