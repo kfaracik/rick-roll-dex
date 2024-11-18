36d6503d9a74a500e5197f7729af867f26bd0b8f
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-18 21:56:46 +0100

    feat(nav-tab): update names

--- src/stacks/TabNavigation/index.tsx
+++ src/stacks/TabNavigation/index.tsx
@@ -9,12 +9,12 @@ export const TabNavigationStack = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name="All characters"
+        name="ALL CHARACTERS"
         component={CharacterListStack}
         options={{headerShown: false}}
       />
       <Tab.Screen
-        name="Liked characters"
+        name="LIKED CHARACTERS"
         component={FavoriteCharactersStack}
         options={{headerShown: false}}
       />
