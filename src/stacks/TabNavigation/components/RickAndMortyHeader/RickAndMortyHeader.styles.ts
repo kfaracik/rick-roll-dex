c9f5fd88212f19850f613917acd9d3b612415bb2
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-19 13:08:21 +0100

    feat(navigation): add RickAndMortyHeader component

---
+++ src/stacks/TabNavigation/components/RickAndMortyHeader/RickAndMortyHeader.styles.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  headerImage: {
+    width: 105,
+    height: 32,
+  },
+});
---
+++ src/stacks/TabNavigation/components/RickAndMortyHeader/index.ts
@@ -0,0 +1,1 @@
+export {RickAndMortyHeader} from './RickAndMortyHeader';
