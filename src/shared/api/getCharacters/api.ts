0110e577673220861510213c97f10ac6705168fa
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-20 12:14:24 +0100

    refactor(api): refactor character api for searching

--- src/shared/api/getCharacters/api.ts
+++ src/shared/api/getCharacters/api.ts
@@ -1,8 +1,13 @@
 import apiClient from '../config';
 
-export const getCharacters = async (page = 1) => {
+export const getCharacters = async (page = 1, searchQuery = '') => {
   try {
-    const response = await apiClient.get(`/character?page=${page}`);
+    const response = await apiClient.get(`/character`, {
+      params: {
+        page,
+        name: searchQuery,
+      },
+    });
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch characters');
--- src/shared/api/getCharacters/query.ts
+++ src/shared/api/getCharacters/query.ts
@@ -1,11 +1,17 @@
-import {useQuery} from 'react-query';
-import {getCharacters} from './api';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getCharacters } from './api';
 
-export const useCharacters = (page = 1) => {
-  return useQuery(['characters', page], () => getCharacters(page), {
-    keepPreviousData: true,
-    onError: error => {
-      console.error('Error fetching characters:', error);
+export const useCharacters = (searchQuery: string) => {
+  return useInfiniteQuery({
+    initialPageParam: 0,
+    queryKey: ['characters', searchQuery],
+    queryFn: ({ pageParam = 1 }) => getCharacters(pageParam, searchQuery),
+    getNextPageParam: (lastPage) => {
+      if (lastPage?.info?.next) {
+        return lastPage.info.next.page;
+      }
+      return undefined;
     },
+    staleTime: 1000 * 60, // 1 minute
   });
 };
