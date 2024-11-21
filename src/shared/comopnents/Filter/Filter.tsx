f482f7f095f29dc4cd77ed4ab737945f5baf9358
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-21 22:28:33 +0100

    fix(filters): fix issue with filters expanded and keyboard visible

--- src/shared/comopnents/Filter/Filter.tsx
+++ src/shared/comopnents/Filter/Filter.tsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
-import {ScrollView, View, Text} from 'react-native';
+import {ScrollView, View, Text, useWindowDimensions} from 'react-native';
 import {styles} from './Filter.styled';
 import {Species, Status} from '../../../shared/api';
 import {Button} from '../Button';
-import {Colors} from '../../utils';
 import {Card} from '../Card';
 import {CheckboxItem} from '../CheckboxItem';
-import Icon from 'react-native-vector-icons/AntDesign';
 import {AnimatedChevron} from '../AnimatedChevron';
+
+const VERTICAL_COMPONENTS_SPACE = 390;
 
 type FilterProps = {
   onApplyFilters: (status: Status, species: Species) => void;
@@ -17,6 +17,9 @@ export const Filter = ({onApplyFilters}: FilterProps) => {
   const [filterOptionsExpanded, setFilterOptionsExpanded] = useState(false);
   const [status, setStatus] = useState<Status>(null);
   const [species, setSpecies] = useState<Species>(null);
+
+  const {height: screenHeight} = useWindowDimensions();
+  const filtersAvailableHeight = screenHeight - VERTICAL_COMPONENTS_SPACE;
 
   const handleStatusChange = (value: Status) => {
     setStatus(value === status ? null : value);
@@ -41,7 +44,7 @@ export const Filter = ({onApplyFilters}: FilterProps) => {
   };
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={[styles.container, {maxHeight: filtersAvailableHeight}]}>
       <Button
         title="FILTER "
         onPress={onFilterButtonPress}
@@ -53,49 +56,55 @@ export const Filter = ({onApplyFilters}: FilterProps) => {
         />
       </Button>
       {filterOptionsExpanded && (
-        <Card style={styles.card}>
-          <View>
-            <Text>STATUS</Text>
-            <CheckboxItem
-              label="Alive"
-              value="Alive"
-              status={status}
-              onPress={handleStatusChange}
-            />
-            <CheckboxItem
-              label="Dead"
-              value="Dead"
-              status={status}
-              onPress={handleStatusChange}
-            />
-            <CheckboxItem
-              label="Unknown"
-              value="Unknown"
-              status={status}
-              onPress={handleStatusChange}
-            />
-          </View>
-          <View>
-            <Text>SPECIES</Text>
-            <CheckboxItem
-              label="Human"
-              value="Human"
-              status={species}
-              onPress={handleSpeciesChange}
-            />
-            <CheckboxItem
-              label="Humanoid"
-              value="Humanoid"
-              status={species}
-              onPress={handleSpeciesChange}
-            />
-          </View>
-          <View style={styles.buttonsContainer}>
-            <Button title="RESET" onPress={onResetFiltersPress} mode="white" />
-            <Button title="APPLY" onPress={onApplyPress} mode="primary" />
-          </View>
-        </Card>
+        <ScrollView style={{maxHeight: filtersAvailableHeight}}>
+          <Card style={styles.card}>
+            <View>
+              <Text>STATUS</Text>
+              <CheckboxItem
+                label="Alive"
+                value="Alive"
+                status={status}
+                onPress={handleStatusChange}
+              />
+              <CheckboxItem
+                label="Dead"
+                value="Dead"
+                status={status}
+                onPress={handleStatusChange}
+              />
+              <CheckboxItem
+                label="Unknown"
+                value="Unknown"
+                status={status}
+                onPress={handleStatusChange}
+              />
+            </View>
+            <View>
+              <Text>SPECIES</Text>
+              <CheckboxItem
+                label="Human"
+                value="Human"
+                status={species}
+                onPress={handleSpeciesChange}
+              />
+              <CheckboxItem
+                label="Humanoid"
+                value="Humanoid"
+                status={species}
+                onPress={handleSpeciesChange}
+              />
+            </View>
+            <View style={styles.buttonsContainer}>
+              <Button
+                title="RESET"
+                onPress={onResetFiltersPress}
+                mode="white"
+              />
+              <Button title="APPLY" onPress={onApplyPress} mode="primary" />
+            </View>
+          </Card>
+        </ScrollView>
       )}
-    </ScrollView>
+    </View>
   );
 };
